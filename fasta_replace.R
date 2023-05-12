c3e81f1037ee68d81cb57d352bdc9216b483efa7
Robert Koch <r.tobiaskoch@gmail.com>
2023-05-12 11:58:50 -0400

    fixed replace name and added missing ids to name_switch_report.csv

--- fasta_replace.R
+++ fasta_replace.R
@@ -4,7 +4,7 @@ rm(list = ls())
 
 # Load packages using pacman::p_load
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load("seqinr", "stringr")
+pacman::p_load("seqinr", "tidyverse")
 
 fn = list.files(path = "data_input/new",
                 pattern = "fasta|fa",
@@ -38,20 +38,18 @@ rm(fn)
 
 pattern = readLines("data_input/new_to_old_pattern_match", warn = F)
 
-#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 #extract the ID's from the new names for matching to the old name which is just the ID
-new_match = str_extract(names(fasta_new), pattern)
-#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-#>#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-#>#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+new_match_pattern = str_extract(names(fasta_new), pattern)
+
 
 #get the location of the sequences in the old list that match to the new list
-index_old = match(new_match, names(fasta_old), nomatch = 0)
+index_old = match(new_match_pattern, names(fasta_old), nomatch = 0)
 
 #check to see if some items didn't match
 if (0 %in% index_old) {
-  print("You have missing IDs in your list.")
+  x = which(index_old == 0)
+  miss_id = names(fasta_new[x])
+  paste("New ID(s)",miss_id, "is missing from your old fasta but was added to fasta_combined.fasta.")
 } else {
   print("All IDs are present in your list.")
 }
@@ -62,14 +60,19 @@ fasta_comb = c(fasta_new, fasta_old[-index_old])
 #get positions of new names in combined file
 index_comb = match(names(fasta_new), names(fasta_comb), nomatch = 0)
 
-#get names of old, new, and combined name for double check
-old_index = index_old
-comb_index = index_comb
-old_nm = names(fasta_old[index_old])
-new_nm = names(fasta_new)
+#get names of new, and combined name for double check
 comb_nm = names(fasta_comb[index_comb])
+new_nm = names(fasta_new)
 
-name_check = data.frame(old_index, comb_index, old_nm, new_nm, comb_nm)
+#create data frame with only matching old names
+index_old_match = subset(index_old, index_old > 0)
+old_nm = names(fasta_old[index_old_match])
+old_nm_df = data.frame(old_nm, index_old_match)
+
+name_check = data.frame(index_old, index_comb, new_match_pattern, comb_nm) %>%
+  left_join(old_nm_df, by = c("index_old" = "index_old_match")) %>%
+  mutate(old_nm = replace_na(old_nm, "missing")) %>%
+  select(index_old, index_comb, old_nm, new_match_pattern, comb_nm)
 
 write.csv(name_check, "data_output/name_switch_report.csv")
 
